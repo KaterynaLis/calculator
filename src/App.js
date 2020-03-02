df4ebc81cde9e8b42d2a0d329b483abb3cd7aced
KaterynaLis <votetolisitsa@gmail.com>
2020-03-02 15:13:32 +0100

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-     
+    <div className="calculator_container">
+    <div className="row">
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>/</button>
     </div>
+
+    <div className="row">
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>*</button>
+    </div>
+
+    <div className="row">
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>+</button>
+    </div>
+
+    <div className="row">
+    <button>.</button>
+    <button>0</button>
+    <button>=</button>
+    <button>-</button>
+    </div>
+  </div>
+  </div>
   );
-}
+}}
 
 export default App;
